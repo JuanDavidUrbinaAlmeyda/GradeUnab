9637b98e8e1368989a9c6b17a17eeb9488fb7a8e
JuanDavidUrbinaAlmeyda <jurbina148@unab.edu.co>
2023-10-18 09:52:22 -0500

    commit2

---
+++ app/src/main/java/com/juanurbina/gradeunab/CreateSubActivity.java
@@ -0,0 +1,14 @@
+package com.juanurbina.gradeunab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateSubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_sub);
+    }
+}
