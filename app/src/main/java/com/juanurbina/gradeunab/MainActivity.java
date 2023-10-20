d8b3d5b3369517b6e23e840d260794fd56dfa813
JuanDavidUrbinaAlmeyda <jurbina148@unab.edu.co>
2023-10-20 09:53:29 -0500

    20/10/2023- TRABAJO EN CLASE: Andrés Parra, Nicolás Gómez, Juan Aguirre, Juan Urbina.

--- app/src/main/java/com/juanurbina/gradeunab/MainActivity.java
+++ app/src/main/java/com/juanurbina/gradeunab/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnCuenta=findViewById(R.id.btnCuenta);
-        Button btnBackMain= findViewById(R.id.btnBackMain);
+        Button btnBackMain= findViewById(R.id.btnBackCredits);
+        Button btnCredits= findViewById(R.id.btnCreditos);
+        Button btnAddSub= findViewById(R.id.btnAñadirMateria);
         btnCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent myIntent2= new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(myIntent2);
+            }
+        });
+        btnCredits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent3= new Intent(MainActivity.this, CreditsActivity.class);
+                startActivity(myIntent3);
+            }
+        });
+        btnAddSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent4= new Intent(MainActivity.this, CreateSubActivity.class);
+                startActivity(myIntent4);
             }
         });
     }
--- app/src/main/java/com/juanurbina/gradeunab/ProfileActivity.java
+++ app/src/main/java/com/juanurbina/gradeunab/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.juanurbina.gradeunab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Button btnBackProfile= findViewById(R.id.btnBackCredits);
+        btnBackProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent= new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
